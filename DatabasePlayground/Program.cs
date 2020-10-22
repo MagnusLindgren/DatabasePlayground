fc233a1fe4a345695f866a69b6d59c4f1178eac1
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2020-10-22 20:04:57 +0200

    Cleaned up in the code. Made method to make the code alot smaller and less clutter

--- DatabasePlayground/Program.cs
+++ DatabasePlayground/Program.cs
@@ -2,6 +2,8 @@
 using System.Xml;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DatabasePlayground
 {
@@ -9,6 +11,7 @@ namespace DatabasePlayground
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(150, 40);
             string connection_string = new SqliteConnectionStringBuilder()
             {
                 DataSource = @"..\..\..\video_games_v03.db",
@@ -21,28 +24,31 @@ namespace DatabasePlayground
                 var command = connection.CreateCommand();
                 command.CommandText =
                     $@"
-                        SELECT Title, Publishers                            
+                        SELECT Title, Publishers, Year                           
                         FROM video_games
                     ";
 
                 using (var reader = command.ExecuteReader())
-                {
-                    int output_lines = 0;
+                {                   
                     string column1 = reader.GetName(0);
                     string column2 = reader.GetName(1);
+                    string column3 = reader.GetName(2);
                     string column1_type = reader.GetDataTypeName(0);
                     string column2_type = reader.GetDataTypeName(1);
-                    Console.Write(column1 + " : " + column1_type);
-                    Console.CursorLeft = Console.WindowWidth / 2;
-                    Console.WriteLine(column2 + " : " + column2_type);
-                    Console.WriteLine("----------------------------------------------------");
+                    string column3_type = reader.GetDataTypeName(2);
+
+                    ColumnOutput(column1, column2, column3);
+                    ColumnOutput(column1_type, column2_type, column3_type);
+                  
+                    int output_lines = 0;
                     while (reader.Read())
                     {                       
                         string gameTitle = reader.GetString(0);
-                        string publisher_if_available = (reader.IsDBNull(1) ? "null" : reader.GetString(1));                       
-                        Console.Write(gameTitle);
-                        Console.CursorLeft = Console.WindowWidth / 2;
-                        Console.WriteLine(publisher_if_available);
+                        string publisher_if_available = (reader.IsDBNull(1) ? "null" : reader.GetString(1));
+                        string year = reader.GetString(2);
+
+                        ColumnOutput(gameTitle, publisher_if_available, year);
+
                         output_lines++;
                         if (output_lines == 10)
                         {
@@ -52,7 +58,12 @@ namespace DatabasePlayground
                         }
                     }
                 }
-            }            
+            }
+            
+            static void ColumnOutput(string in1, string in2, string in3)
+            {
+                Console.WriteLine($"{in1,-50}" +  $"{in2, -20}" + $"{in3, 10}");                
+            }
         }
     }
 }
